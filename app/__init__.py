71dd9432fe3ac3c8973e83de74870efa31528640
Dharsh076 <dharshinivasudevan99@gmail.com>
2025-07-24 13:56:52 -0400

    Fix: Auto-create DB tables on startup

--- app/__init__.py
+++ app/__init__.py
@@ -9,15 +9,16 @@ csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdb.sqlite3'
 
     db.init_app(app)
-    login_manager.init_app(app)
-    csrf.init_app(app)
 
-    login_manager.login_view = 'main.login'  # this is important!
+    with app.app_context():
+        from . import models
+        db.create_all()
 
-    from .routes import main
-    app.register_blueprint(main)
+    from .routes import auth
+    app.register_blueprint(auth)
 
     return app
